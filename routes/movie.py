9db2e560c4cd6851fcb7bf1fc8ecc4239198e6cd
Leejaewon2 <dlwodnjs2669@gmail.com>
2023-12-12 21:04:42 +0900

    재원 get_movie release_date 추가

--- routes/movie.py
+++ routes/movie.py
@@ -2,7 +2,7 @@ import requests
 import json
 from bs4 import BeautifulSoup
 from flask import jsonify
-import re
+
 def get_movie():
     url = 'https://search.daum.net/search?nil_suggest=btn&w=tot&DA=SBC&q=%EC%98%81%ED%99%94%EC%88%9C%EC%9C%84'
 
@@ -23,7 +23,12 @@ def get_movie():
         movieRank = movieInfo.find('span', attrs={'class': 'img_number'})
         movieTitle = movieInfo.find('a', attrs={'class': 'tit_main'})
         movieScore = movieInfo.find('em', attrs={'class': 'rate'})
-        movieOpenDate = movieInfo.find('dd', class_='cont')
+        cont_elements = movieInfo.find_all('dd', class_='cont')
+
+        if len(cont_elements) >= 2:
+            movieOpenDate = cont_elements[1].get_text(strip=True)
+        else:
+            movieOpenDate = "-"
 
         movie_data.append({
             'rank': movieRank.get_text() if movieRank else "-",
@@ -34,14 +39,14 @@ def get_movie():
 
         top_10_movies = movie_data[:10]
         print("제발 잘나와라 : ", release_date)
-
+        print("너는 잘 나오니..?", cont_elements)
     # Convert the movie data to JSON
     json_data = json.dumps(top_10_movies, ensure_ascii=False, indent=4)
     return json_data
 
 def get_ott_movie():
     try:
-        url = 'https://movie.daum.net/ranking/ott'
+        url = 'https://movie.daum.net/moviedb/grade?movieId=143538'
 
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
@@ -49,29 +54,32 @@ def get_ott_movie():
 
         response = requests.get(url, headers=headers)  # headers were missing in your GET request
         soup = BeautifulSoup(response.text, 'html.parser')
-        # 특정 클래스를 가진 모든 ol 태그를 찾음
-        ol_elements = soup.find_all('ol', class_='list_movieranking aniposter_ott') if soup.find('ol', attrs={
-            'list_movieranking aniposter_ott'}) else []
+        # 제목 가져오기
+        title = soup.find('span', class_='txt_tit').get_text(strip=True)
+
+        # 개봉일 가져오기
+        release_date = soup.find('dt', text='개봉').find_next('dd').get_text(strip=True)
+
+        # 평점 가져오기
+        rating = soup.find('dt', text='평점').find_next('dd').get_text(strip=True)
 
         # 모든 영화 정보를 담을 리스트 초기화
         movie_data = []
 
-        for ol_element in ol_elements:
-            movieInfoList = ol_element.find_all('li')
+        for main_element in main_elements:
 
-            for movieInfo in movieInfoList:
-                movieTitle = movieInfo.find('a', class_='link_txt')
-                movieRank = movieInfo.find('span', class_='rank_num')
-                movieScore = movieInfo.find('span', class_='info_grade')
+            movieTitle = title.find('span', class_='txt_tit')
+            movieScore = release_date.find('dd', class_='rank_num')
+            moviereleaseDate = rating.find('span', class_='info_grade')
 
-                # 영화 정보를 딕셔너리에 추가
-                movie_data.append({
-                    'rank': movieRank.get_text() if movieRank else "-",
-                    'title': movieTitle.get_text().strip() if movieTitle else "-",
-                    'score': movieScore.get_text() if movieScore else "-",
-                })
+            # 영화 정보를 딕셔너리에 추가
+            movie_data.append({
+                'rank': movieScore.get_text() if movieScore else "-",
+                'title': movieTitle.get_text().strip() if movieTitle else "-",
+                'score': moviereleaseDate.get_text() if moviereleaseDate else "-",
+            })
 
-                # 전체 영화 리스트에 추가
+            # 전체 영화 리스트에 추가
 
         # 순서대로 10개씩 추출하여 리스트 생성
         tving_movies = movie_data[:10]
